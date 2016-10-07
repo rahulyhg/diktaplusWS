6d4816d97961ad17208e1c705792c617490603aa
Gerardo Medina <gerardo.medina103@alu.ulpgc.es>
2016-10-07 15:25:47 +0100

    Create list texts

--- src/DiktaplusBundle/Controller/DefaultController.php
+++ src/DiktaplusBundle/Controller/DefaultController.php
@@ -24,55 +24,11 @@ class DefaultController extends Controller
 
     public function textsAction()
     {
-        return $this->render('DiktaplusBundle:Default:texts.html.twig');
-    }
-
-    public function signupAction(Request $request) {
-        $admin_user = new Admin();
-        $signup_form = $this->createForm(new SignupType(), $admin_user);
-        $signup_form->handleRequest($request);
-
-        if ($signup_form->isSubmitted()) {
-            $email = $signup_form->get('email')->getData();
-            $password = $this->get('security.encoder_factory')->getEncoder($admin_user)->encodePassword($signup_form->get('password')->getData(), $admin_user->getSalt());
-
-            $admin_user->setEmail($email);
-            $admin_user->setPassword($password);
-        }
-
-        if ($signup_form->isValid()) {
-            $email_exists = false;
-
-            if ($email_exists== false) {
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($admin_user);
-                $em->flush();
-
-                $this->session->getFlashBag()->add('info', 'Signup successfully done');
-                return $this->redirect($this->generateURL('welcome'));
-            } else {
-                $this->session->getFlashBag()->add('info', 'Email already used');
-            }
-        }
-        return $this->render('DiktaplusBundle:Default:signup.html.twig',
-            array('signup_form' => $signup_form->createView()));
+        $repository = $this->getDoctrine()
+            ->getRepository('DiktaplusBundle:Text');
+        $texts = $repository->findAll();
+        return $this->render('DiktaplusBundle:Default:texts.html.twig',array('texts' => $texts));
     }
 
 
-    public function loginAction(Request $request){
-        $authenticationUtils = $this->get('security.authentication_utils');
-        $error = $authenticationUtils->getLastAuthenticationError();
-
-        $securityContext = $this->container->get('security.authorization_checker');
-        if (!$error && $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            $this->session->getFlashBag()->add('info', 'Login done');
-            return $this->redirect($this->generateURL('welcome'));
-        } else if ($error){
-            $this->session->getFlashBag()->add('info', 'Check email and password');
-            return $this->render('DiktaplusBundle:Default:login.html.twig');
-        } else {
-            return $this->render('DiktaplusBundle:Default:login.html.twig');
-        }
-
-    }
 }
---
+++ src/DiktaplusBundle/Controller/SecurityController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace DiktaplusBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use DiktaplusBundle\Entity\Admin;
+use DiktaplusBundle\Form\Type\SignupType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\SecurityContext;
+
+
+class SecurityController extends Controller
+{
+    private $session;
+    public function __construct() {
+        $this->session = new Session();
+    }
+
+    public function signupAction(Request $request) {
+        $admin_user = new Admin();
+        $signup_form = $this->createForm(new SignupType(), $admin_user);
+        $signup_form->handleRequest($request);
+
+        if ($signup_form->isSubmitted()) {
+            $email = $signup_form->get('email')->getData();
+            $password = $this->get('security.encoder_factory')->getEncoder($admin_user)->encodePassword($signup_form->get('password')->getData(), $admin_user->getSalt());
+
+            $admin_user->setEmail($email);
+            $admin_user->setPassword($password);
+        }
+
+        if ($signup_form->isValid()) {
+            $email_exists = false;
+
+            if ($email_exists== false) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($admin_user);
+                $em->flush();
+
+                $this->session->getFlashBag()->add('info', 'Signup successfully done');
+                return $this->redirect($this->generateURL('welcome'));
+            } else {
+                $this->session->getFlashBag()->add('info', 'Email already used');
+            }
+        }
+        return $this->render('DiktaplusBundle:Default:signup.html.twig',
+            array('signup_form' => $signup_form->createView()));
+    }
+
+
+    public function loginAction(Request $request){
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $securityContext = $this->container->get('security.authorization_checker');
+        if (!$error && $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $this->session->getFlashBag()->add('info', 'Login done');
+            return $this->redirect($this->generateURL('welcome'));
+        } else if ($error){
+            $this->session->getFlashBag()->add('info', 'Check email and password');
+            return $this->render('DiktaplusBundle:Default:login.html.twig');
+        } else {
+            return $this->render('DiktaplusBundle:Default:login.html.twig');
+        }
+
+    }
+}
