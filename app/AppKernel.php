80c6fde3bad0d7b7e5c97a86481f21166ca7ec68
Gerardo Medina <gerardo.medina103@alu.ulpgc.es>
2016-10-08 12:03:54 +0100

    Add FOS rest API

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            new FOS\RestBundle\FOSRestBundle(),
+
             new DiktaplusBundle\DiktaplusBundle(),
         );
 
