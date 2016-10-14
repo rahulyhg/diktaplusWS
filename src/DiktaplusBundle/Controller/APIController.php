6f724c9a27e354279e79ff26319ab1ff5f271321
Gerardo Medina <gerardo.medina103@alu.ulpgc.es>
2016-10-14 12:37:20 +0100

    API REST for users work

--- src/DiktaplusBundle/Controller/APIController.php
+++ src/DiktaplusBundle/Controller/APIController.php
@@ -15,24 +15,21 @@ class APIController extends FOSRestController
 {
     public function postUserAction(Request $request) {
 
-        if ($request->getMethod()=="POST") {;
-            $data = json_decode($request->getContent(), true);
-            $user = new User();
+        $data = json_decode($request->getContent(), true);
+        $user = new User();
 
-            $user->setUsername($data['username']);
-            $user->setEmail($data['email']);
-            $user->setCountry($data['country']);
-            $user->setPassword($data['password']);
+        $user->setUsername($data['username']);
+        $user->setEmail($data['email']);
+        $user->setCountry($data['country']);
+        $user->setPassword($data['password']);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
-            return new Response('User successfully added');
-        }
-        $response = new Response("Error");
-        $response->setStatusCode(500);
-        return $response;
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        return new Response('User successfully added');
+
     }
+
     public function getUserAction($id) {
         $repository = $this->getDoctrine()
             ->getRepository('DiktaplusBundle:User');
@@ -47,6 +44,27 @@ class APIController extends FOSRestController
         $view->setFormat("json");
         return $this->handleView($view);
     }
+
+    public function putUserAction(Request $request, $id) {
+
+        $data = json_decode($request->getContent(), true);
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('DiktaplusBundle:User')->find($id);
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No user found for id ' . $id
+            );
+        }
+        $user->setUsername($data['username']);
+        $user->setEmail($data['email']);
+        $user->setCountry($data['country']);
+        $user->setPassword($data['password']);
+
+        $em->flush();
+        return new Response('User successfully modified');
+
+    }
+
     public function deleteUserAction($id) {
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository('DiktaplusBundle:User')->find($id);
