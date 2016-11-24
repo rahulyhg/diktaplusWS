24785104f4bfdff29fc33484293bb7a0379736bf
Gerardo Medina <gera.mdlh@gmail.com>
2016-11-24 20:57:09 +0000

    Command create admin

--- src/DiktaplusBundle/Command/CreateAdminCommand.php
+++ src/DiktaplusBundle/Command/CreateAdminCommand.php
@@ -4,6 +4,7 @@ namespace DiktaplusBundle\Command;
 
 use DiktaplusBundle\Entity\Admin;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -12,15 +13,17 @@ class CreateAdminCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-            ->setName('diktaplus:update-admin')
-            ->setDescription('Updates the credentials of the admin');
+            ->setName('diktaplus:create-admin')
+            ->setDescription('Creates a new admin')
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the admin.')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password of the admin.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $new_admin = new Admin();
-        $new_admin->setEmail("email@email.com");
-        $password = $this->getContainer()->get('security.encoder_factory')->getEncoder($new_admin)->encodePassword('pass', $new_admin->getSalt());
+        $new_admin->setEmail($input->getArgument('email'));
+        $password = $this->getContainer()->get('security.encoder_factory')->getEncoder($new_admin)->encodePassword($input->getArgument('password'), $new_admin->getSalt());
         $new_admin->setPassword($password);
         $em = $this->getContainer()->get('doctrine')->getEntityManager();
         $em->persist($new_admin);
